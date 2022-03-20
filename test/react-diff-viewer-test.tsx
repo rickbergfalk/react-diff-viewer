339230dc034c22c4b81076188ff47556723cb502
Rick Bergfalk <rbergfalk@montecarlodata.com>
2022-03-20 12:48:33 -0500

    Tests run off builds

--- test/react-diff-viewer-test.tsx
+++ test/react-diff-viewer-test.tsx
@@ -1,7 +1,6 @@
 import { shallow } from "enzyme";
 import * as React from "react";
 import * as expect from "expect";
-// @ts-expect-error
 import DiffViewer from "../lib/index";
 
 const oldCode = `
